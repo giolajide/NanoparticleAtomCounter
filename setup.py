7fd6429c4f2f13179ec7deaf1350cb593e79eac6
giolajide <141545135+giolajide@users.noreply.github.com>
2026-02-09 08:15:48 -0600

    Update streamlit version requirement from 1.4 to 1.50

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "numpy>=1.20,<2.3",
         "pandas>=1.4",
-        "streamlit>=1.4,<2",
+        "streamlit>=1.50,<2",
         "openpyxl>=3.1",
         "xlrd>=2.0",
     ],
